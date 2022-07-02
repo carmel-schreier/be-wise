1b8e5528e4aee59a09207955136ed7570d6749de
carmel-schreier <89860235+carmel-schreier@users.noreply.github.com>
2022-07-02 13:02:21 +0300

    profile-card component

--- client/src/app/lecturers/lecturers.module.ts
+++ client/src/app/lecturers/lecturers.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LecturersComponent } from './lecturers.component';
+import { ProfileCardComponent } from './profile-card/profile-card.component';
 
 
 
 @NgModule({
   declarations: [
-    LecturersComponent
+    LecturersComponent,
+    ProfileCardComponent
   ],
   imports: [
     CommonModule
