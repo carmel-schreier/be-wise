e15b87153c109a2b49daa8e05a7d723d51e48711
carmel-schreier <89860235+carmel-schreier@users.noreply.github.com>
2022-07-02 13:00:21 +0300

    added client repo with angular + lecturers module & component

---
+++ client/src/app/lecturers/lecturers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lecturers',
+  templateUrl: './lecturers.component.html',
+  styleUrls: ['./lecturers.component.sass']
+})
+export class LecturersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ client/src/app/lecturers/lecturers.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LecturersComponent } from './lecturers.component';
+
+
+
+@NgModule({
+  declarations: [
+    LecturersComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LecturersModule { }
