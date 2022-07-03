681a6513a0ce12aa529ecff7d63be0c8e282434b
carmel-schreier <89860235+carmel-schreier@users.noreply.github.com>
2022-07-03 07:39:34 +0300

    added costom pipe for experience

--- client/src/app/lecturers/lecturers.module.ts
+++ client/src/app/lecturers/lecturers.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LecturersComponent } from './lecturers.component';
 import { ProfileCardComponent } from './profile-card/profile-card.component';
+import { ExperiencePipe } from '../pipes/experience.pipe';
 
 @NgModule({
-  declarations: [LecturersComponent, ProfileCardComponent],
+  declarations: [LecturersComponent, ProfileCardComponent, ExperiencePipe],
   imports: [CommonModule],
 })
 export class LecturersModule {}
