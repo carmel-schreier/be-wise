405944fb7449d56de3beff1c34885260fa4e82f9
carmel-schreier <89860235+carmel-schreier@users.noreply.github.com>
2022-07-03 07:42:32 +0300

    added costom pipe for experience

--- client/src/app/pipes/experience.pipe.ts
+++ client/src/app/pipes/experience.pipe.ts
@@ -6,11 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ExperiencePipe implements PipeTransform {
   transform(value: any): any {
     const currentYear: any = new Date().getFullYear();
-    console.log('currentYear:' + currentYear);
     const startingYear: any = new Date(value).getFullYear();
-    console.log('startingYear:' + startingYear);
     let period = Math.round(currentYear - startingYear);
-    console.log('period:' + period);
     let text: string =
       period < 1 ? `One of our new recruits!` : `${period} Years of experience`;
     return text;
